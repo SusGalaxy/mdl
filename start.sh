fc2eaeedb0ed195bd8cf57a2f9f2d8fdb8db4fe7
applerobloxgames <109912460+InikoMatthewPro@users.noreply.github.com>
2024-02-11 16:05:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ history -c
 rm -rf ~/.bash_history
 cpuid -1 | grep -E "avx|avx2|avx512|sha|vaes|ryzen|sse2|sse4.2"
 grep flags /proc/cpuinfo | grep -oE "\b(avx|avx2|avx512|sha|vaes|sse2|sse4_2)\b"
+grep flags /proc/cpuinfo | head -n 1 | tr ' ' '\n' | grep -E "avx|avx2|avx512|sha|vaes|ryzen|sse2|sse4_2"
+lscpu | grep -E "Flags|AVX|AVX2|AVX-512|SHA|VAES|Ryzen|SSE2|SSE4\.2"
